d7375472dc35dcb581133170618b44ae491eb304
M Rosyadi Aulawi <dadi15122001@gmail.com>
2025-03-04 12:44:12 +0800

    edit porto

---
+++ script.js
@@ -0,0 +1,16 @@
+// Smooth scroll
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
+
+const menuBtn = document.getElementById("menu-btn");
+const mobileMenu = document.getElementById("mobile-menu");
+
+menuBtn.addEventListener("click", () => {
+  mobileMenu.classList.toggle("hidden");
+});
