293bec987cd064f679abe4a1cae4899bacae2211
M Rosyadi Aulawi <dadi15122001@gmail.com>
2025-03-07 11:00:38 +0800

    update

--- script.js
+++ script.js
@@ -17,3 +17,15 @@ menuBtn.addEventListener("click", () => {
 
 // Script untuk mengatur tahun secara otomatis
 document.getElementById("year").textContent = new Date().getFullYear();
+
+function openModal(imageSrc) {
+  const modal = document.getElementById("imageModal");
+  const modalImage = document.getElementById("modalImage");
+  modalImage.src = imageSrc;
+  modal.classList.remove("hidden");
+}
+
+function closeModal() {
+  const modal = document.getElementById("imageModal");
+  modal.classList.add("hidden");
+}
