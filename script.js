0c067a44692bd5ed9c81b364fa566371875490d6
M Rosyadi Aulawi <dadi15122001@gmail.com>
2025-03-19 11:54:54 +0800

    update

--- script.js
+++ script.js
@@ -29,3 +29,16 @@ function closeModal() {
   const modal = document.getElementById("imageModal");
   modal.classList.add("hidden");
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const navLinks = document.querySelectorAll("nav ul li a");
+
+  function setActiveLink(event) {
+    navLinks.forEach((link) => link.classList.remove("active"));
+    event.target.classList.add("active");
+  }
+
+  navLinks.forEach((link) => {
+    link.addEventListener("click", setActiveLink);
+  });
+});
